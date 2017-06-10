6f89f2422b474ef344987147a3532b9163081a1b
RobertoAnacletoCosta <racosta@globo.com>
2017-06-10 19:04:56 -0400

    Inclui itens de cotroller ignorados

---
+++ SIGESF/Areas/Administracao/Controllers/DetalheSolicitacaoSuprimentoFundosController.cs
@@ -0,0 +1,132 @@
+﻿using SIGESF.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace SIGESF.Areas.Administracao.Controllers
+{
+    public class DetalheSolicitacaoSuprimentoFundosController : Controller
+    {
+        private ContextoEF db = new ContextoEF();
+
+        // GET: Administracao/DetalheSolicitacaoSuprimentoFundos
+        public ActionResult Index()
+        {
+            var detalheSolicitacoes = db.DetalheSolicitacoes.Include(d => d.NaturezasDespesas).Include(d => d.SolicitacaoSuprimentoFundos);
+            return View(detalheSolicitacoes.ToList());
+        }
+
+        // GET: Administracao/DetalheSolicitacaoSuprimentoFundos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DetalheSolicitacaoSuprimentoFundos detalheSolicitacaoSuprimentoFundos = db.DetalheSolicitacoes.Find(id);
+            if (detalheSolicitacaoSuprimentoFundos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(detalheSolicitacaoSuprimentoFundos);
+        }
+
+        // GET: Administracao/DetalheSolicitacaoSuprimentoFundos/Create
+        public ActionResult Create()
+        {
+            ViewBag.NaturezaDespesaId = new SelectList(db.NaturezasDespesas, "NaturezaDespesaId", "Descricao");
+            ViewBag.SolicitacaoSuprimentoFundosId = new SelectList(db.SolicitacoesSF, "SolicitacaoSuprimentoFundosId", "Proc_Proad");
+            return View();
+        }
+
+        // POST: Administracao/DetalheSolicitacaoSuprimentoFundos/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DetalheSolicitacaoId,SolicitacaoSuprimentoFundosId,NaturezaDespesaId,ValorSolicitado")] DetalheSolicitacaoSuprimentoFundos detalheSolicitacaoSuprimentoFundos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DetalheSolicitacoes.Add(detalheSolicitacaoSuprimentoFundos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.NaturezaDespesaId = new SelectList(db.NaturezasDespesas, "NaturezaDespesaId", "Descricao", detalheSolicitacaoSuprimentoFundos.NaturezaDespesaId);
+            ViewBag.SolicitacaoSuprimentoFundosId = new SelectList(db.SolicitacoesSF, "SolicitacaoSuprimentoFundosId", "Proc_Proad", detalheSolicitacaoSuprimentoFundos.SolicitacaoSuprimentoFundosId);
+            return View(detalheSolicitacaoSuprimentoFundos);
+        }
+
+        // GET: Administracao/DetalheSolicitacaoSuprimentoFundos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DetalheSolicitacaoSuprimentoFundos detalheSolicitacaoSuprimentoFundos = db.DetalheSolicitacoes.Find(id);
+            if (detalheSolicitacaoSuprimentoFundos == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.NaturezaDespesaId = new SelectList(db.NaturezasDespesas, "NaturezaDespesaId", "Descricao", detalheSolicitacaoSuprimentoFundos.NaturezaDespesaId);
+            ViewBag.SolicitacaoSuprimentoFundosId = new SelectList(db.SolicitacoesSF, "SolicitacaoSuprimentoFundosId", "Proc_Proad", detalheSolicitacaoSuprimentoFundos.SolicitacaoSuprimentoFundosId);
+            return View(detalheSolicitacaoSuprimentoFundos);
+        }
+
+        // POST: Administracao/DetalheSolicitacaoSuprimentoFundos/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DetalheSolicitacaoId,SolicitacaoSuprimentoFundosId,NaturezaDespesaId,ValorSolicitado")] DetalheSolicitacaoSuprimentoFundos detalheSolicitacaoSuprimentoFundos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(detalheSolicitacaoSuprimentoFundos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.NaturezaDespesaId = new SelectList(db.NaturezasDespesas, "NaturezaDespesaId", "Descricao", detalheSolicitacaoSuprimentoFundos.NaturezaDespesaId);
+            ViewBag.SolicitacaoSuprimentoFundosId = new SelectList(db.SolicitacoesSF, "SolicitacaoSuprimentoFundosId", "Proc_Proad", detalheSolicitacaoSuprimentoFundos.SolicitacaoSuprimentoFundosId);
+            return View(detalheSolicitacaoSuprimentoFundos);
+        }
+
+        // GET: Administracao/DetalheSolicitacaoSuprimentoFundos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DetalheSolicitacaoSuprimentoFundos detalheSolicitacaoSuprimentoFundos = db.DetalheSolicitacoes.Find(id);
+            if (detalheSolicitacaoSuprimentoFundos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(detalheSolicitacaoSuprimentoFundos);
+        }
+
+        // POST: Administracao/DetalheSolicitacaoSuprimentoFundos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            DetalheSolicitacaoSuprimentoFundos detalheSolicitacaoSuprimentoFundos = db.DetalheSolicitacoes.Find(id);
+            db.DetalheSolicitacoes.Remove(detalheSolicitacaoSuprimentoFundos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SIGESF/Areas/Administracao/Controllers/SolicitacaoSuprimentoFundosController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SIGESF.Models;
+
+namespace SIGESF.Areas.Administracao.Controllers
+{
+    public class SolicitacaoSuprimentoFundosController : Controller
+    {
+        private ContextoEF db = new ContextoEF();
+
+        // GET: Administracao/SolicitacaoSuprimentoFundos
+        public ActionResult Index()
+        {
+            var solicitacoesSF = db.SolicitacoesSF.Include(s => s.AgenteSuprido);
+            return View(solicitacoesSF.ToList());
+        }
+
+        // GET: Administracao/SolicitacaoSuprimentoFundos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SolicitacaoSuprimentoFundos solicitacaoSuprimentoFundos = db.SolicitacoesSF.Find(id);
+            if (solicitacaoSuprimentoFundos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(solicitacaoSuprimentoFundos);
+        }
+
+        // GET: Administracao/SolicitacaoSuprimentoFundos/Create
+        public ActionResult Create()
+        {
+            ViewBag.AgenteSupridoId = new SelectList(db.AgentesSupridos, "AgenteSupridoId", "Nome");
+            return View();
+        }
+
+        // POST: Administracao/SolicitacaoSuprimentoFundos/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SolicitacaoSuprimentoFundosId,Proc_Proad,DataSolicitacaoSF,Finalidade,AgenteSupridoId")] SolicitacaoSuprimentoFundos solicitacaoSuprimentoFundos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SolicitacoesSF.Add(solicitacaoSuprimentoFundos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AgenteSupridoId = new SelectList(db.AgentesSupridos, "AgenteSupridoId", "Nome", solicitacaoSuprimentoFundos.AgenteSupridoId);
+            return View(solicitacaoSuprimentoFundos);
+        }
+
+        // GET: Administracao/SolicitacaoSuprimentoFundos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SolicitacaoSuprimentoFundos solicitacaoSuprimentoFundos = db.SolicitacoesSF.Find(id);
+            if (solicitacaoSuprimentoFundos == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AgenteSupridoId = new SelectList(db.AgentesSupridos, "AgenteSupridoId", "Nome", solicitacaoSuprimentoFundos.AgenteSupridoId);
+            return View(solicitacaoSuprimentoFundos);
+        }
+
+        // POST: Administracao/SolicitacaoSuprimentoFundos/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SolicitacaoSuprimentoFundosId,Proc_Proad,DataSolicitacaoSF,Finalidade,AgenteSupridoId")] SolicitacaoSuprimentoFundos solicitacaoSuprimentoFundos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(solicitacaoSuprimentoFundos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AgenteSupridoId = new SelectList(db.AgentesSupridos, "AgenteSupridoId", "Nome", solicitacaoSuprimentoFundos.AgenteSupridoId);
+            return View(solicitacaoSuprimentoFundos);
+        }
+
+        // GET: Administracao/SolicitacaoSuprimentoFundos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SolicitacaoSuprimentoFundos solicitacaoSuprimentoFundos = db.SolicitacoesSF.Find(id);
+            if (solicitacaoSuprimentoFundos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(solicitacaoSuprimentoFundos);
+        }
+
+        // POST: Administracao/SolicitacaoSuprimentoFundos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SolicitacaoSuprimentoFundos solicitacaoSuprimentoFundos = db.SolicitacoesSF.Find(id);
+            db.SolicitacoesSF.Remove(solicitacaoSuprimentoFundos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
